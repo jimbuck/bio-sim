9dd2ba0ebb50dd02a5d724aaae2fe661e469c08c
Jim Buck <jim@jimmyboh.com>
2016-05-24 17:07:51 -0400

    Temporarily removed reference of phaser ([see issue here](https://github.com/photonstorm/phaser/issues/2492)).

--- _references.d.ts
+++ _references.d.ts
@@ -1,1 +1,7 @@
-/// <reference path="./node_modules/phaser/typescript/phaser.comments.d.ts" />
+
+/// <reference path="./node_modules/@angular/common/index.d.ts" />
+/// <reference path="./node_modules/@angular/core/index.d.ts" />
+/// <reference path="./node_modules/@angular/http/index.d.ts" />
+/// <reference path="./node_modules/@angular/router/index.d.ts" />
+/// <reference path="./node_modules/@angular/platform-browser/index.d.ts" />
+
