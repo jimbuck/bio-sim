f369c2baf559f39eb2d3a0f0b29aafad777d7732
Jim Buck <jim@jimmyboh.com>
2017-06-12 17:45:31 -0400

    Delete everything to make way for electron-forge usage.

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-
-// Load libraries and kickoff the app...
-
-import * as $ from 'jquery';
-window.jQuery = window.$ = $;
-import 'bootstrap';
-
-import './app';
--- src/missing-definitions.d.ts
+++
@@ -1,5 +0,0 @@
-
-interface Window {
-	jQuery: JQueryStatic;
-	$: JQueryStatic;
-}
